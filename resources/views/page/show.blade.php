37f1d86ec9edbdf9b88efa21f24e3216efd576c2
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-24 14:46:30 +0200

    composer update

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -1,8 +1,2 @@
-@extends('layouts.app')
-
-@section('body')
-    <div class="container my-8 mx-auto text-white">
-        <h1>{{ $page->title }}</h1>
-        {!! PageArchitect::render($page->body) !!}
-    </div>
-@endsection
+<h1>{{ $page->title }}</h1>
+{!! PageArchitect::render($page->body) !!}
